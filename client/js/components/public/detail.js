f87c82e493ca7a9d02049af77ac3785214d3152f
Ubuntu <ryan@ryanjirani.ryanjirani.c7.internal.cloudapp.net>
2015-02-22 10:21:05 +0000

    akdkdssl

--- client/js/components/public/detail.js
+++ client/js/components/public/detail.js
@@ -1,6 +1,10 @@
 var React = require('react'),
   Router = require('react-router'),
-  DetailPicture = require('./detail-picture');
+  mui = require('material-ui'),
+  Paper = mui.Paper,
+  DetailPicture = require('./detail-picture'),
+  FunderList = require('./funderlist'),
+  Information = require('./information');
 
 var DetailPage = React.createClass({
 
@@ -9,7 +13,11 @@ var DetailPage = React.createClass({
     return ( 
       <main id = "detail-page" >
         <DetailPicture imgUrl="http://www.independent.co.uk/incoming/article8465213.ece/alternates/w620/v2-cute-cat-picture.jpg" />
-      </main>
+        <FunderList />
+        <Paper z-Depth={2}>
+            <Information />
+        </Paper>
+        </main>
     );
   }
 });
